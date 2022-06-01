6a26019fc3a66a328227b9fc6f0eb9dfb7d46b4c
macheng <kylinmac@163.com>
2022-06-01 14:24:30 +0800

    add

--- flink-jar/src/main/java/com/mc/flink/func/SourceFunction.java
+++ flink-jar/src/main/java/com/mc/flink/func/SourceFunction.java
@@ -6,12 +6,12 @@ import java.util.concurrent.atomic.AtomicLong;
 
 public class SourceFunction implements BaseFunction {
 
-    AtomicLong atomicLong=new AtomicLong(0);
+
 
     @Override
     public String apply() {
 
-        return atomicLong.getAndIncrement()+"";
+        return "source";
 
     }
 }
