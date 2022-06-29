2d58a867219efc306842520e6ad4e1d36f7c0f92
macheng <hk@123456>
2022-06-29 19:14:26 +0800

    add

--- flink-jar/src/main/java/com/mc/flink/func/MyFunc.java
+++ flink-jar/src/main/java/com/mc/flink/func/MyFunc.java
@@ -1,6 +1,8 @@
 package com.mc.flink.func;
 
+import com.alibaba.fastjson.JSONObject;
 import com.baomidou.mybatisplus.core.toolkit.StringUtils;
+import com.mc.flink.entity.TestEntity;
 import com.mc.flink.udf.TestClassPath;
 import com.mc.flink.utils.ObjFactory;
 import org.apache.flink.streaming.api.functions.ProcessFunction;
@@ -15,6 +17,8 @@ public class MyFunc extends ProcessFunction<String, String> implements Serializa
     public void processElement(String string, ProcessFunction<String, String>.Context context, Collector<String> collector) throws Exception {
         TestClassPath testClassPath = new TestClassPath();
         System.out.println(LocalDateTime.now()+" process :");
+        TestEntity testEntity = JSONObject.parseObject("{}", TestEntity.class);
+        System.out.println(testEntity);
         testClassPath.output();
         collector.collect(ObjFactory.getJson().toString());
         String s="test_camel";
